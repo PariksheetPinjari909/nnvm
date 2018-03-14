00ed285c83b1cd5ce488e8d3ec78fa8936d06453
pariksheet.pinjari@huawei.com <pariksheet.pinjari@huawei.com>
2018-03-14 10:26:12 +0530

    Yolo region boxes added as part of tutorial folder

--- src/top/vision/shortcut.cc
+++ src/top/vision/shortcut.cc
@@ -14,24 +14,24 @@ namespace nnvm {
 namespace top {
 
 NNVM_REGISTER_OP(shortcut)
-    .describe(R"code(Shortcut layer (sometimes called Shortcut Connections).
-                Shortcut connections are the connections which skip one or more layers.
-                )code" NNVM_ADD_FILELINE)
-    .set_num_inputs(2)
-    .set_num_outputs(1)
-    .set_support_level(5)
-    .add_argument("lhs", "Tensor", "first input")
-    .add_argument("rhs", "Tensor", "second input")
-    .set_attr<FInferType>("FInferType", ShortcutType<2, 1>)
-    .set_attr<FInferShape>("FInferShape", ShortcutShape<2, 1>)
-    .set_attr<FInplaceOption>(
-        "FInplaceOption",
-        [](const NodeAttrs &attrs) {
-          return std::vector<std::pair<int, int>>{{0, 0}, {1, 0}};
-        })
-    .set_attr<FGradient>("FGradient", [](const NodePtr &n,
-                                         const std::vector<NodeEntry> &ograds) {
-      return std::vector<NodeEntry>{ograds[0], ograds[0]};
-    });
+.describe(R"code(Shortcut layer (sometimes called Shortcut Connections).
+Shortcut connections are the connections which skip one or more layers.
+)code" NNVM_ADD_FILELINE)
+.set_num_inputs(2)
+.set_num_outputs(1)
+.set_support_level(5)
+.add_argument("lhs", "Tensor", "first input")
+.add_argument("rhs", "Tensor", "second input")
+.set_attr<FInferType>("FInferType", ShortcutType<2, 1>)
+.set_attr<FInferShape>("FInferShape", ShortcutShape<2, 1>)
+.set_attr<FInplaceOption>(
+    "FInplaceOption",
+    [](const NodeAttrs &attrs) {
+      return std::vector<std::pair<int, int>>{{0, 0}, {1, 0}};
+    })
+.set_attr<FGradient>("FGradient", [](const NodePtr &n,
+                                     const std::vector<NodeEntry> &ograds) {
+  return std::vector<NodeEntry>{ograds[0], ograds[0]};
+});
 }  // namespace top
 }  // namespace nnvm
