55a38bbada1cc0205bccd202fa916c16e0a487ae
PariksheetPinjari909 <pariksheet.pinjari@huawei.com>
2018-04-01 23:24:30 +0530

    resnet50/resnet152 model opertor reworked

--- python/nnvm/top/vision.py
+++ python/nnvm/top/vision.py
@@ -8,20 +8,6 @@ import tvm
 from . import registry as reg
 from .registry import OpPattern
 
-@reg.register_compute("darknet_shortcut")
-def compute_shortcut(attrs, inputs, _):
-    """Compute definition of shortcut"""
-    out = topi.vision.shortcut(inputs[0], inputs[1])
-    return out
-
-@reg.register_schedule("darknet_shortcut")
-def schedule_shortcut(attrs, outs, target):
-    """Schedule definition of shortcut"""
-    with tvm.target.create(target):
-        return topi.generic.schedule_injective(outs)
-
-reg.register_pattern("shortcut", OpPattern.INJECTIVE)
-
 @reg.register_compute("yolo2_reorg")
 def compute_reorg(attrs, inputs, _):
     """Compute definition of reorg"""
